b6f40ac74522b59672bdfbae957b4b5fefdad44a
Kiet Pham <kiet.pham2610@gmail.com>
2023-01-10 19:44:58 +1100

    add port 3000 as default

--- backend/app.ts
+++ backend/app.ts
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv';
-const dotenvResult = dotenv.config();
-if (dotenvResult.error) {
-    throw dotenvResult.error;
-}
-
-import express from 'express';
-import * as http from 'http';
-
-import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
-import cors from 'cors';
+import * as http from 'http';
+import * as winston from 'winston';
+
 import { CommonRoutesConfig } from './common/common.routes.config';
 import { CoursesRoutes } from './courses/course.routes.config';
+import cors from 'cors';
 import debug from 'debug';
+import dotenv from 'dotenv';
+import express from 'express';
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
-const port = process.env.DEFAULT_PORT;
+const port = process.env.DEFAULT_PORT || '3000';
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
 
